f2eaf2fb652ef12bd8e27403180f3467cbad88fc
Kevin Wittmer <wittmerkevinr@gmail.com>
2017-11-26 20:44:39 -0500

    KW: Added functionality to create a new DAGR

--- web/mmda/urls.py
+++ web/mmda/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'mmda'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^insert_file/$', views.insert_file, name='insert_file')
 ]
--- web/mmda/views.py
+++ web/mmda/views.py
@@ -1,4 +1,8 @@
+import os
+
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 from .models import DataAggregate
 
@@ -6,3 +10,8 @@ def index(request):
     data_aggregates_list = DataAggregate.objects.all()
     context = { 'data_aggregates_list': data_aggregates_list }
     return render(request, 'mmda/index.html', context)
+
+def insert_file(request):
+    new_dagr = DataAggregate(name=os.path.basename(request.POST['file_path']))
+    new_dagr.save()
+    return HttpResponseRedirect(reverse('mmda:index'))
