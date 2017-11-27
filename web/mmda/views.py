a426d379b52285a1a81931f4223400dbffe83342
Kevin Wittmer <wittmerkevinr@gmail.com>
2017-11-26 22:00:35 -0500

    KW: Added functionality to create new FileMetadata record when DAGR is created

--- web/mmda/views.py
+++ web/mmda/views.py
@@ -1,10 +1,11 @@
+import datetime
 import os
 
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import DataAggregate
+from .models import DataAggregate, FileMetadata
 
 def index(request):
     data_aggregates_list = DataAggregate.objects.all()
@@ -12,6 +13,22 @@ def index(request):
     return render(request, 'mmda/index.html', context)
 
 def insert_file(request):
-    new_dagr = DataAggregate(name=os.path.basename(request.POST['file_path']))
+    # Grab the file path from the HTTP request
+    file_path = request.POST['file_path']
+
+    # Create the DAGR where the default name is the selected file's name
+    new_dagr = DataAggregate(name=os.path.basename(file_path))
     new_dagr.save()
+
+    # Populate the file metadata for the selected file
+    metadata = FileMetadata(
+        file_name=os.path.basename(file_path),
+        storage_path=os.path.dirname(file_path),
+        creator_name=os.getlogin(),
+        time_created=datetime.datetime.fromtimestamp(os.path.getctime(file_path)),
+        last_modified=datetime.datetime.fromtimestamp(os.path.getmtime(file_path)),
+        document_type_id=0)
+    metadata.save()
+
+    # Redirect the user back to the home page
     return HttpResponseRedirect(reverse('mmda:index'))
