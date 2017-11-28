6ee9025b644aefdbec3eff1f6885969704284a0a
Kevin Wittmer <wittmerkevinr@gmail.com>
2017-11-28 17:35:23 -0500

    KW: Implemented bulk data entry task

--- web/mmda/urls.py
+++ web/mmda/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'mmda'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^insert_file/$', views.insert_file, name='insert_file')
+    url(r'^insert_file/$', views.insert_file, name='insert_file'),
+    url(r'^bulk_data_insert/$', views.bulk_data_insert, name='bulk_data_insert')
 ]
--- web/mmda/views.py
+++ web/mmda/views.py
@@ -1,4 +1,5 @@
 import datetime
+import glob
 import os
 
 from django.db import connection
@@ -51,6 +52,49 @@ def insert_file(request):
 	            (SELECT MAX(id) FROM mmda_dataaggregate),
                 (SELECT MAX(id) FROM mmda_filemetadata)
             )""")
+
+    # Redirect the user back to the home page
+    return HttpResponseRedirect(reverse('mmda:index'))
+
+def bulk_data_insert(request):
+    # Grab the folder path from the HTTP request
+    folder_path = request.POST['folder_path']
+    
+    with connection.cursor() as cursor:
+        # Create a new DAGR where the default name is the folder's name
+        cursor.execute("""
+            INSERT INTO mmda_dataaggregate (
+                name, time_created
+            ) VALUES (
+	            %s, %s
+            )
+        """, [os.path.basename(folder_path) + '/', datetime.datetime.now()])
+
+        for file_path in glob.glob(os.path.join(folder_path, '*.*')):
+            # Create a new FileMetadata entry
+            cursor.execute("""
+                INSERT INTO mmda_filemetadata (
+                    file_name, storage_path, creator_name, time_created, last_modified, document_type_id, size
+                ) VALUES (
+                    %s, %s, %s, %s, %s, %s, %s
+                )""", [
+                os.path.basename(file_path),
+                os.path.dirname(file_path),
+                os.getlogin(),
+                datetime.datetime.fromtimestamp(os.path.getctime(file_path)),
+                datetime.datetime.fromtimestamp(os.path.getmtime(file_path)),
+                0,
+                os.path.getsize(file_path)
+            ])
+
+            # Map the newly created FileMetadata to the new DAGR
+            cursor.execute("""
+                INSERT INTO mmda_dataaggregate_files (
+                    dataaggregate_id, filemetadata_id
+                ) VALUES (
+                    (SELECT MAX(id) FROM mmda_dataaggregate),
+                    (SELECT MAX(id) FROM mmda_filemetadata)
+                )""")
 
     # Redirect the user back to the home page
     return HttpResponseRedirect(reverse('mmda:index'))
